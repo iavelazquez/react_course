a75801997c89e722c32e27f2051c8eecb6f9e8a2
iavelazquez <developer@IM-M10-150.local>
2017-10-12 15:31:46 -0700

    Adding buttons.

--- exercise1.js
+++ exercise1.js
@@ -1,40 +1,46 @@
-// Using https://jscomplete.com/repl/ to run this exercise.
-
 // Write JavaScript here and press Ctrl+Enter to execute
 
 class Button extends React.Component { 
-	render() {
-  	return (
-    	<button onClick={this.props.onClickFunc}>
-      	+1
-      </button>
-    );
-  }
-}
+    
+    handleClick = () => {
+        this.props.onClickFunc( this.props.incrementValue )
+    }
 
+    render() {
+        return (
+        <button onClick={this.handleClick}>
+            +{this.props.incrementValue}
+        </button>
+    );
+    }
+}
+    
 const Result = (props) => {
-	return (
-    	<div>{props.label}</div>
-  );
-}
-
-class App extends React.Component {
-	state = { counter: 0};
-  
-  incrementCounter = () => {
-  	this.setState((prevState) => ({
-    	counter : prevState.counter + 1
-    }));
-  }
-  
-	render() {
-  	return (
-    	<div>
-      	<Button onClickFunc={this.incrementCounter}/>
-        <Result label={this.state.counter} />
-      </div>
+    return (
+        <div>{props.label}</div>
     );
-  }
+}
+    
+class App extends React.Component {
+    state = { counter: 0};
+    
+    incrementCounter = (incrementValue) => {
+        this.setState((prevState) => ({
+        counter : prevState.counter + incrementValue
+    }));
+    }
+    
+    render() {
+        return (
+        <div>
+            <Button incrementValue={1} onClickFunc={this.incrementCounter}/>
+            <Button incrementValue={5} onClickFunc={this.incrementCounter}/>
+            <Button incrementValue={10} onClickFunc={this.incrementCounter}/>
+            <Button incrementValue={100} onClickFunc={this.incrementCounter}/>
+        <Result label={this.state.counter} />
+        </div>
+    );
+    }
 }
 
 ReactDOM.render(<App/>, mountNode);
